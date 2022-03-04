54c6da42d1e9ab39d230d7e7f921f6999ebee76f
amrabouelenin <amr@new-track.net>
2022-03-04 17:06:16 +0100

    broken our app into components

---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.scss'
+import Directory from '../../components/diretory/directory';
+const HomePage = () => {
+  return (
+    <div className='homepage'>
+     <Directory/>
+    </div>
+  );
+}
+
+export default HomePage;
